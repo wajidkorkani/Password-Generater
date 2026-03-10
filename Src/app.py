02921b36f389ecc5a1319261a535f03d3bf02222
wajidkorkani <errormrcoder@gmail.com>
2026-03-11 00:04:50 +0500

    Password has been created and it is working and generating uniqe passwords.

--- Src/app.py
+++ Src/app.py
@@ -1,3 +1,5 @@
+import random
+
 list_of_lists = [
     ['!','@','#','$','%','^','&','*','(',')','-','+','=','{','}','[',']','|',':',';','<','>','.','?','/'],
     [1,2,3,4,5,6,7,8,9,0],
@@ -10,3 +12,14 @@ list_of_lists = [
     'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
     ]
 ]
+
+passwordList = ""
+length = 8
+
+while(length > 0):
+    list_number = random.randint(0, 3)
+    charecter_number = random.randint(0, len(list_of_lists[list_number]) - 1)
+    passwordList += str(list_of_lists[list_number][charecter_number])
+    length -= 1
+
+print(passwordList)
